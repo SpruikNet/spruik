04fffa715649261f26674c4e366d78dbd5079581
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-02 17:14:30 +0530

    publisher application view

---
+++ src/components/MainContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import MainTopbar from './MainTopbar'
+import ApplicationContainer from './ApplicationContainer'
+
+
+class MainContainer extends Component {
+render() {
+return ( 
+
+ <div className='ui grid'>
+        <MainTopbar />
+        <ApplicationContainer />
+      </div>
+    )
+  }
+}
+
+export default MainContainer
+ 
