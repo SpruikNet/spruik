b22e2f9239afb7faddbb0a3e7895a1db071bc48c
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-03 15:51:52 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
+import MainSidebar from './components/MainSidebar'
+import MainContainer from './components/MainContainer
+
+
 import './App.css'
- class App extends Component {
+class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <h2>Welcome to React</h2>
+        <div className='ui grid stackable'>
+          <div
+            className='MainSidebarWrap column four wide'>
+            <MainSidebar />
+          </div>
+          <div className='MainContainerWrap column twelve wide'>
+            <MainContainer />
+          </div>
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     )
   }
-}
- export default App
+} 
+
+
+
+export default App
