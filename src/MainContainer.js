d5575f104590feec431dabae034888b18113b697
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-03 16:12:33 +0530

    setup routing

---
+++ src/MainContainer.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+import { HashRouter as Router, Route } from 'react-router-dom'
+
+import MainTopbar from './MainTopbar'
+import ApplicationContainer from './ApplicationContainer'
+import DomainsContainer from './DomainsContainer'
+
+class MainContainer extends Component {
+  render () {
+    return (
+      <Router>
+        <div className='ui grid'>
+          <MainTopbar />
+          <Route path='/' exact={true} component={ApplicationContainer} />
+          <Route path='/apply' exact={true} component={ApplicationContainer} />
+          <Route path='/domains' exact={true} component={DomainsContainer} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default MainContainer
+
