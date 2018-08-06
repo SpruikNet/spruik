5e35533dc9e01325d2b0a235770467a81cfb0d41
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-06 18:57:06 +0530

    toastr

--- src/components/PublisherApplicationForm.js
+++ src/components/PublisherApplicationForm.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react'
-
 import './PublisherApplicationForm.css'
-
+import toastr from 'toastr'
+import IsValidDomain from 'is-valid-domain'
 class PublisherApplicationForm extends Component {
   render () {
     return (
@@ -13,65 +12,150 @@ class PublisherApplicationForm extends Component {
             </div>
           </div>
           <div className='column sixteen wide left aligned'>
-          <p>Please complete the information below in order to apply to the adChain Registry.
-          Remember, the total ADT staked on your behalf will be the exact amount needed
-          for adToken holders to challenge your application.
-          </p>
+         
+
+<div className='ui large header'>
+              PUBLISHER APPLICATION
+            </div>
           </div>
           <div className='column sixteen wide left aligned'>
-            <form className='ui form'>
+            <p>Please complete the information below in order to apply to the adChain Registry.
+          Remember, the total ADT staked on your behalf will be the exact amount needed
+          for adToken holders to challenge your application.
+            </p>
+          </div>
+          <div className='column sixteen wide left aligned'>
+            <form
+              onSubmit={this.onFormSubmit.bind(this)}
+              className='ui form'>
               <div className='field required'>
                 <label>Domain</label>
                 <div className='ui input'>
-                  <input type='text' placeholder='example.com' />
+                  <input
+                    type='text'
+                    placeholder='example.com'
+                    name='domain'
+                    required
+                  />
                 </div>
               </div>
               <div className='field'>
                 <label>Site Name</label>
                 <div className='ui input'>
-                  <input type='text' placeholder='Site Name' />
+                  <input
+                    type='text'
+                    placeholder='Site Name'
+                    name='siteName'
+                  />
                 </div>
               </div>
               <div className='field'>
                 <label>Country Based In</label>
                 <div className='ui input'>
-                  <input type='text' placeholder='United States' />
+                  <input
+                    type='text'
+                    placeholder='United States'
+                    name='country'
+                  />
                 </div>
               </div>
               <div className='two fields'>
                 <div className='field'>
                   <label>First Name</label>
                   <div className='ui input'>
-                    <input type='text' placeholder='John' />
+                    <input
+                      type='text'
+                      placeholder='John'
+                      name='firstName'
+                    />
                   </div>
                 </div>
                 <div className='field'>
                   <label>Last Name</label>
                   <div className='ui input'>
-                    <input type='text' placeholder='Doe' />
+                    <input
+                      type='text'
+                      placeholder='Doe'
+                      name='lastName'
+                    />
                   </div>
                 </div>
               </div>
               <div className='field'>
                 <label>Email Address</label>
                 <div className='ui input'>
-                  <input type='text' placeholder='john@example.com' />
+                  <input
+                    type='text'
+                    placeholder='john@example.com'
+                    name='email'
+                  />
                 </div>
               </div>
               <div className='field required'>
                 <label>Total ADT to Stake</label>
                 <div className='ui input'>
-                  <input type='text' placeholder='100' />
+                  <input
+                    type='text'
+                    placeholder='100'
+                    name='stake'
+                    required
+                  />
                 </div>
               </div>
               <div className='field'>
-                <button className='ui blue submit button'>APPLY</button>
+                <button
+                  type='submit'
+                  className='ui blue submit button'>
+                  APPLY
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
     )
+  }
+
+  onFormSubmit (event) {
+    event.preventDefault()
+    const {target} = event
+
+    const domain = target.domain.value
+    const siteName = target.siteName.value
+    const country = target.country.value
+    const firstName = target.firstName.value
+    const lastName = target.lastName.value
+    const email = target.email.value
+    const stake = parseInt(target.stake.value, 10)
+
+    if (!isValidDomain(domain)) {
+      toastr.error('Invalid domain')
+      return false
+    }
+
+    if (!isValidEmail(email)) {
+      toastr.error('Invalid email')
+      return false
+    }
+
+    if (!stake || stake <= 0) {
+      toastr.error('ADT Stake must be greater than 0')
+      return false
+    }
+
+    this.save({
+      domain,
+      siteName,
+      country,
+      firstName,
+      lastName,
+      email,
+      stake
+    })
+  }
+
+  save (data) {
+    toastr.success('Submitted')
   }
 }
 
