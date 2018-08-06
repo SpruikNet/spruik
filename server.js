3d85e779a44ab043629511e2e7c608a61fd4a274
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-06 13:46:27 +0530

    enforce https

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const server = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
-
+const enforeceHttps = require(koa-sslify')
 app.use(server('$ {_dirname}/build'))
+app,use(enforceHttps())
+
+app.use(serve('${_dirname}/build))
 
 const port = process.env.PORT || 3000
 
