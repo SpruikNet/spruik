593636e930ae1d7d8938b2725523d198bd7ceec6
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-06 15:57:14 +0530

    info panel +disqus

---
+++ src/components/DomainProfileActionContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+ import './DomainProfileActionContainer.css'
+ class DomainProfileActionContainer extends Component {
+  render () {
+    return (
+      <div className='DomainProfileActionContainer BoxFrame'>
+      action
+      </div>
+    )
+  }
+}
+ export default DomainProfileActionContainer
+
