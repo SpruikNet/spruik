71a0d8957f328233eb775fb78bcf573e46346a04
Gaurav Savanur <gauravsavanur07@gmail.com>
2018-08-14 14:29:37 +0530

    salt gen + countdown

--- src/components/DomainVoteRevealContainer.js
+++ src/components/DomainVoteRevealContainer.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import commafy from 'commafy'
 import toastr from 'toastr'
 import moment from 'moment'
-import DomainVoteRevealInContainer from './DomainVoteRevealInProgressContainer'
+import { Radio } from 'semantic-ui-react'
+
+import Countdown from './CountdownText'
+import registry from '../services/registry'
+import DomainVoteRevealInProgressContainer from './DomainVoteRevealInProgressContainer'
+import StatProgressBar from './StatProgressBar'
 
 import './DomainVoteRevealContainer.css'
 
@@ -11,123 +17,221 @@ class DomainVoteRevealContainer extends Component {
     super()
 
     this.state = {
-  domain: props.domain,
+      domain: props.domain,
       applicationExpiry: null,
       votesFor: 0,
-      votesAgainst: 0
- commitEndDate: null,
+      votesAgainst: 0,
+      commitEndDate: null,
       revealEndDate: null,
       inProgress: false,
       didChallenge: false,
-      salt: 123,
-      voteOption: 0
-
-
-
-    }	    }
-     this.getListing()
-    this.getPoll() 
-    this.getChallenge()
-
-  }	  }
-
+      salt: null,
+      voteOption: null
     }
+
+    this.onVoteOptionChange = this.onVoteOptionChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+
+    this.getListing()
+    this.getPoll()
+    this.getChallenge()
   }
 
   render () {
-    const blocksRemaining = 1239
- const {
-      applicationExpiry,
-      domain,
+    const {
       votesFor,
+      votesAgainst,
       revealEndDate,
       inProgress,
       didChallenge,
-      votesAgainst
+      voteOption
     } = this.state
-     const stageEnd = revealEndDate ? moment.unix(revealEndDate).format('YYYY-MM-DD HH:mm:ss') : '-'
-const totalVotes = ((votesFor + votesAgainst) | 0)
+
+    const stageEndMoment = revealEndDate ? moment.unix(revealEndDate) : null
+    const stageEnd = stageEndMoment ? stageEndMoment.format('YYYY-MM-DD HH:mm:ss') : '-'
+
+    // "N | 0" coerces to int
+    const totalVotes = ((votesFor + votesAgainst) | 0)
     const supportFill = ((totalVotes / votesFor * 1e2) | 0)
     const opposeFill = ((totalVotes / votesAgainst * 1e2) | 0)
 
     return (
-      <div className='DomainChallengeContainer'>
+      <div className='DomainVoteRevealContainer'>
         <div className='ui grid stackable'>
           <div className='column sixteen wide'>
-            <strong>VOTING – REVEAL</strong>
+            <div className='ui large header center aligned'>
+              VOTING – REVEAL
+            </div>
           </div>
+          {didChallenge ? <div className='column sixteen wide'>
+            <div className='ui message warning'>
+              You've challenged this domain.
+            </div>
+          </div>
+          : null}
           <div className='column sixteen wide'>
             <p>
 The first phase of the voting process is the commit phase where the ADT holder stakes a hidden amount of ADT to SUPPORT or OPPOSE the domain application. The second phase is the reveal phase where the ADT holder reveals the staked amount of ADT to either the SUPPORT or OPPOSE side.
             </p>
           </div>
-          <div className='column sixteen wide center aligned'>
-            <div className='ui divider' />
+          <div className='ui divider' />
+          <div className='column sixteen wide center aligned ProgressContainer'>
             <p>
-          Total ADT already committed by the general ADT community:
+              ADT holders have revealed their vote to show:
             </p>
-            <p>
-              <strong>99,000 ADT</strong>
-            </p>
-            <div className='ui divider' />
-            <p>
-          Blocks remaining until reveal period ends
-            </p>
-            <p><strong>{commafy(blocksRemaining)} blocks</strong></p>
-            <p><small>or approximately: 02 days, 14 hours, and 49 minutes</small></p>
-            <div className='ui divider' />
+            <div className='BarContainer'>
+              <StatProgressBar
+                fills={[supportFill, opposeFill]}
+                showFillLabels
+                showLegend
+                fillLabels={['SUPPORT', 'OPPOSE']}
+              />
+            </div>
+            <div className='Breakdown'>
+              <div className='BreakdownItem'>
+                <div className='BreakdownItemBox'></div>
+                <span className='BreakdownItemLabel'>{commafy(votesFor)} ADT</span>
+              </div>
+              <div className='BreakdownItem'>
+                <div className='BreakdownItemBox'></div>
+                <span className='BreakdownItemLabel'>{commafy(votesAgainst)} ADT</span>
+              </div>
+            </div>
           </div>
+          <div className='ui divider' />
           <div className='column sixteen wide center aligned'>
-            <p>
-              Your latest commit was <strong>10,000 ADT</strong> to <strong>OPPOSE</strong>
-the Publisher’s application into the adChain Registry
-            </p>
-            <button className='ui button blue'>
-              REVEAL
-            </button>
+            <div className='ui message info'>
+              <p>
+            Reveal stage ends
+              </p>
+              <p><strong>{stageEnd}</strong></p>
+              <p>Remaning time: <Countdown endDate={stageEndMoment} /></p>
+            </div>
+          </div>
+          <div className='ui divider' />
+          <div className='column sixteen wide center aligned'>
+            <form
+              onSubmit={this.onFormSubmit}
+              className='ui form'>
+              <div className='ui field'>
+                <label>Salt</label>
+                <div className='ui input small'>
+                  <input
+                    type='text'
+                    placeholder='words'
+                    onKeyUp={event => this.setState({salt: parseInt(event.target.value, 10)})}
+                  />
+                </div>
+              </div>
+              <div className='ui two fields VoteOptions'>
+                <div className='ui field'>
+                  <Radio
+                    label='SUPPORT'
+                    name='voteOption'
+                    value='1'
+                    checked={this.state.voteOption === 1}
+                    onChange={this.onVoteOptionChange}
+                  />
+                </div>
+                <div className='ui field'>
+                  <Radio
+                    label='OPPOSE'
+                    name='voteOption'
+                    value='0'
+                    checked={this.state.voteOption === 0}
+                    onChange={this.onVoteOptionChange}
+                  />
+                </div>
+              </div>
+              <div className='ui field'>
+                <button
+                  type="submit"
+                  className={`ui button ${voteOption === 1 ? 'blue' : (voteOption === 0 ? 'purple' : 'disabled')}`}>
+                  {voteOption === null ?
+                    <span>Select Vote Option</span> :
+                    <span>REVEAL {voteOption ? 'SUPPORT' : 'OPPOSE'} VOTE</span> }
+                </button>
+              </div>
+            </form>
           </div>
         </div>
+        {inProgress ? <DomainVoteRevealInProgressContainer /> : null}
       </div>
     )
   }
- async getListing () {
+  onVoteOptionChange (event, { value }) {
+    this.setState({
+      voteOption: parseInt(value, 10)
+    })
+  }
+  async getListing () {
     const {domain} = this.state
     const listing = await registry.getListing(domain)
-     const {
+    const {
       applicationExpiry
     } = listing
-     this.setState({
+    this.setState({
       applicationExpiry
     })
   }
-   async getPoll () {
+  async getPoll () {
     const {domain} = this.state
     const {
       votesFor,
-      votesAgainst
+      votesAgainst,
+      commitEndDate,
+      revealEndDate
     } = await registry.getChallengePoll(domain)
-     this.setState({
+    this.setState({
       votesFor,
-      votesAgainst
+      votesAgainst,
+      commitEndDate,
+      revealEndDate
     })
   }
-   async onReveal (event) {
-    event.preventDefault()
-     const {domain} = this.state
-    const salt = 123
-    const voteOption = 1
-     try {
-      const result = await registry.revealVote({domain, voteOption, salt})
-      toastr.success('Success')
+  async getChallenge () {
+    const {domain} = this.state
+    try {
+      const didChallenge = await registry.didChallenge(domain)
+      this.setState({
+        didChallenge
+      })
     } catch (error) {
-      toastr.error(error.message)
+      toastr.error(error)
     }
   }
-}	}
-
-
-
-
-
-
+  onFormSubmit (event) {
+    event.preventDefault()
+    this.reveal()
+  }
+  async reveal () {
+    const {domain, salt, voteOption} = this.state
+    if (!salt) {
+      toastr.error('Please enter salt value')
+      return false
+    }
+    if (voteOption === null) {
+      toastr.error('Please select a vote option')
+      return false
+    }
+    this.setState({
+      inProgress: true
+    })
+    try {
+      await registry.revealVote({domain, voteOption, salt})
+      toastr.success('Success')
+      this.setState({
+        inProgress: false
+      })
+    } catch (error) {
+      toastr.error(error.message)
+      this.setState({
+        inProgress: false
+      })
+    }
+  }
+}
+DomainVoteRevealContainer.propTypes = {
+  domain: PropTypes.string
+}
 export default DomainVoteRevealContainer
--- src/services/registry.js
+++ src/services/registry.js
@@ -1,14 +1,23 @@
 import sha3 from 'solidity-sha3'
 import pify from 'pify'
-import plcr from './plcr'
+import keyMirror from 'key-mirror'
+import wait from 'promise-wait'
 
-import saltHashVote from './parameterizer'
-
+import store from '../store'
 import token from './token'
+import plcr from './plcr'
 import parameterizer from './parameterizer'
-const {registry:address} = require('../config/address.json')
-
+import saltHashVote from '../utils/saltHashVote'
+const { registry: address } = require('../config/address.json')
 const abi = require('../config/registry.json').abi
+
+const parameters = keyMirror({
+  minDeposit: null,
+  applyStageLen: null,
+  voteQuorum: null,
+  commitPeriodLen: null,
+  revealPeriodLen: null
+})
 
 class RegistryService {
   constructor () {
@@ -25,10 +34,20 @@ class RegistryService {
 
     if (!this.registry) {
       this.registry = window.web3.eth.contract(abi).at(this.address)
+
+      this.forceMine()
     }
   }
 
-  async apply (domain) {
+  getAccount () {
+    if (!window.web3) {
+      return null
+    }
+
+    return window.web3.eth.defaultAccount
+  }
+
+  async apply (domain, deposit = 0) {
     return new Promise(async (resolve, reject) => {
       if (!domain) {
         reject(new Error('Domain is required'))
@@ -39,8 +58,6 @@ class RegistryService {
         this.initContract()
       }
 
-      const minDeposit = 100
-
       const exists = await this.applicationExists(domain)
 
       if (exists) {
@@ -48,12 +65,27 @@ class RegistryService {
         return false
       }
 
-      const approveTx = await token.approve(this.address, minDeposit)
-      await this.getTransactionReceipt(approveTx)
+      try {
+        await token.approve(this.address, deposit)
+        await this.forceMine()
+      } catch (error) {
+        reject(error)
+        return false
+      }
 
-      const result = await pify(this.registry.apply)(domain)
+      try {
+        await pify(this.registry.apply)(domain, deposit)
+      } catch (error) {
+        reject(error)
+        return false
+      }
 
-      resolve(result)
+      store.dispatch({
+        type: 'REGISTRY_DOMAIN_APPLY',
+        domain
+      })
+
+      resolve()
     })
   }
 
@@ -68,17 +100,79 @@ class RegistryService {
         this.initContract()
       }
 
-      const minDeposit = 100
+      let minDeposit = 0
 
-      const approveTx = await token.approve(this.address, minDeposit)
-      await this.getTransactionReceipt(approveTx)
+      try {
+        minDeposit = await this.getMinDeposit()
+      } catch (error) {
+        reject(error)
+        return false
+      }
 
-      const tx = await pify(this.registry.challenge)(domain)
+      try {
+        await token.approve(this.address, minDeposit)
+        await this.forceMine()
+        await this.forceMine()
+      } catch (error) {
+        reject(error)
+        return false
+      }
 
-      const receipt = await this.getTransactionReceipt(tx)
-      const challengeId = parseInt(receipt.logs[1].data, 16)
+      let tx = null
 
-      resolve(challengeId)
+      try {
+        tx = await pify(this.registry.challenge)(domain)
+        await this.forceMine()
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      try {
+        const receipt = await this.getTransactionReceipt(tx)
+        const challengeId = parseInt(receipt.logs[1].data, 16)
+
+        store.dispatch({
+          type: 'REGISTRY_DOMAIN_CHALLENGE',
+          domain
+        })
+
+        resolve(challengeId)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async didChallenge (domain) {
+    return new Promise(async (resolve, reject) => {
+      if (!domain) {
+        reject(new Error('Domain is required'))
+        return false
+      }
+
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      let challengeId = null
+
+      try {
+        challengeId = await this.getChallengeId(domain)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      try {
+        const challenge = await this.getChallenge(challengeId)
+        const isChallenger = (challenge.challenger === this.getAccount())
+        resolve(isChallenger)
+      } catch (error) {
+        reject(error)
+        return false
+      }
     })
   }
 
@@ -93,13 +187,17 @@ class RegistryService {
         this.initContract()
       }
 
-      const exists = await pify(this.registry.appExists.call)(domain)
-
-      resolve(exists)
+      try {
+        const exists = await pify(this.registry.appExists)(domain)
+        resolve(exists)
+      } catch (error) {
+        reject(error)
+        return false
+      }
     })
   }
 
-  async getListing(domain) {
+  async getListing (domain) {
     return new Promise(async (resolve, reject) => {
       if (!domain) {
         reject(new Error('Domain is required'))
@@ -110,23 +208,28 @@ class RegistryService {
         this.initContract()
       }
 
-      const hash = sha3(domain)
+      try {
+        const hash = sha3(domain)
+        const result = await pify(this.registry.listingMap)(hash)
 
-      const result = await pify(this.registry.listingMap.call)(hash)
+        const map = {
+          applicationExpiry: result[0].toNumber(),
+          isWhitelisted: result[1],
+          ownerAddress: result[2],
+          currentDeposit: result[3].toNumber(),
+          challengeId: result[4].toNumber()
+        }
 
-      const map = {
-        applicationExpiry: result[0].toNumber(),
-        isWhitelisted: result[1],
-        ownerAddress: result[2],
-        currentDeposit: result[3].toNumber(),
-        challengeID: result[4].toNumber()
+        resolve(map)
+        return false
+      } catch (error) {
+        reject(error)
+        return false
       }
-
-      resolve(map)
     })
   }
 
-  async getChallenge(challengeId) {
+  async getChallenge (challengeId) {
     return new Promise(async (resolve, reject) => {
       if (!challengeId) {
         reject(new Error('Challenge ID is required'))
@@ -137,22 +240,48 @@ class RegistryService {
         this.initContract()
       }
 
-      const result = await pify(this.registry.challengeMap.call)(challengeId)
+      try {
+        const challenge = await pify(this.registry.challengeMap)(challengeId)
+        const map = {
+          // (remaining) pool of tokens distributed amongst winning voters
+          rewardPool: challenge[0] ? challenge[0].toNumber() : 0,
+          // owner of challenge
+          challenger: challenge[1],
+          // indication of if challenge is resolved
+          resolved: challenge[2],
+          // number of tokens at risk for either party during challenge
+          stake: challenge[3] ? challenge[3].toNumber() : 0,
+          // (remaining) amount of tokens used for voting by the winning side
+          totalTokens: challenge[4]
+        }
 
-      const map = {
-        // (remaining) pool of tokens distributed amongst winning voters
-        rewardPool: result[0].toNumber(),
-        // owner of challenge
-        challenger: result[1],
-        // indication of if challenge is resolved
-        resolved: result[2],
-        // number of tokens at risk for either party during challenge
-        stake: result[3].toNumber(),
-        // (remaining) amount of tokens used for voting by the winning side
-        totalTokens: result[4]
+        resolve(map)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async getChallengeId (domain) {
+    return new Promise(async (resolve, reject) => {
+      if (!domain) {
+        return new Error('Domain is required')
       }
 
-      resolve(map)
+      try {
+        const listing = await this.getListing(domain)
+
+        const {
+          challengeId
+        } = listing
+
+        resolve(challengeId)
+        return false
+      } catch (error) {
+        reject(error)
+        return false
+      }
     })
   }
 
@@ -167,9 +296,40 @@ class RegistryService {
         this.initContract()
       }
 
-      const whitelisted = await pify(this.registry.isWhitelisted.call)(domain)
+      try {
+        const whitelisted = await pify(this.registry.isWhitelisted)(domain)
+        resolve(whitelisted)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
 
-      resolve(whitelisted)
+  async updateStatus (domain) {
+    return new Promise(async (resolve, reject) => {
+      if (!domain) {
+        reject(new Error('Domain is required'))
+        return false
+      }
+
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      try {
+        const result = await pify(this.registry.updateStatus)(domain)
+
+        store.dispatch({
+          type: 'REGISTRY_DOMAIN_UPDATE_STATUS',
+          domain
+        })
+
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
     })
   }
 
@@ -184,20 +344,245 @@ class RegistryService {
         this.initContract()
       }
 
-      const value = await parameterizer.get(name)
-
-      resolve(value)
+      try {
+        const value = await parameterizer.get(name)
+        resolve(value)
+      } catch (error) {
+        reject(error)
+        return false
+      }
     })
   }
-  async getChallengeId (domain) {
-    if (!domain) {
-      return new Error('Domain is required')
-    }
-     const listing = await this.getListing(domain)
-     const {
-      challengeId
-    } = listing
-     return challengeId
+
+  getParameterKeys () {
+    return Promise.resolve(parameters)
+  }
+
+  getMinDeposit () {
+    return this.getParameter('minDeposit')
+  }
+
+  async getCurrentBlockNumber () {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      const result = await pify(window.web3.eth.getBlockNumber)()
+
+      resolve(result)
+    })
+  }
+
+  async getCurrentBlockTimestamp () {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      try {
+        const result = await pify(window.web3.eth.getBlock)('latest')
+
+        resolve(result.timestamp)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async getPlcrAddress () {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      try {
+        const result = await pify(this.registry.voting)()
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async commitPeriodActive (domain) {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      let pollId = null
+
+      try {
+        pollId = await this.getChallengeId(domain)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      if (!pollId) {
+        resolve(false)
+        return false
+      }
+
+      try {
+        const result = await plcr.commitPeriodActive(pollId)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async revealPeriodActive (domain) {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      if (!domain) {
+        reject(new Error('Domain is required'))
+        return false
+      }
+
+      let pollId = null
+
+      try {
+        pollId = await this.getChallengeId(domain)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      if (!pollId) {
+        resolve(false)
+        return false
+      }
+
+      try {
+        const result = await plcr.revealPeriodActive(pollId)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async commitVote ({domain, votes, voteOption, salt}) {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      if (!domain) {
+        reject(new Error('Domain is required'))
+        return false
+      }
+
+      let challengeId = null
+
+      try {
+        challengeId = await this.getChallengeId(domain)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      try {
+        const prevPollId = 0
+        const hash = saltHashVote(voteOption, salt)
+
+        const result = await plcr.commit({pollId: challengeId, hash, tokens: votes, prevPollId})
+
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  async revealVote ({domain, voteOption, salt}) {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      let challengeId = null
+
+      try {
+        challengeId = await this.getChallengeId(domain)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+
+      try {
+        const result = plcr.reveal({pollId: challengeId, voteOption, salt})
+        await this.forceMine()
+
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  getChallengePoll (domain) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const challengeId = await this.getChallengeId(domain)
+        const result = await plcr.getPoll(challengeId)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  pollEnded (domain) {
+    return new Promise(async (resolve, reject) => {
+      const challengeId = await this.getChallengeId(domain)
+
+      if (!challengeId) {
+        resolve(false)
+        return false
+      }
+
+      try {
+        const result = await plcr.pollEnded(challengeId)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
+  }
+
+  voterHasEnoughVotingTokens (tokens) {
+    return plcr.hasEnoughTokens(tokens)
+  }
+
+  async getTransaction (tx) {
+    return new Promise(async (resolve, reject) => {
+      if (!this.registry) {
+        this.initContract()
+      }
+
+      try {
+        const result = await pify(window.web3.eth.getTransaction)(tx)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
+      }
+    })
   }
 
   async getTransactionReceipt (tx) {
@@ -206,27 +591,44 @@ class RegistryService {
         this.initContract()
       }
 
-      const result = await pify(window.web3.eth.getTransactionReceipt)(tx)
-
-      resolve(result)
-    })
-  }
- async getPlcrAddress () {
-    return new Promise(async (resolve, reject) => {
-      if (!this.registry) {
-        this.initContract()
+      try {
+        const result = await pify(window.web3.eth.getTransactionReceipt)(tx)
+        resolve(result)
+      } catch (error) {
+        reject(error)
+        return false
       }
-       const result = await pify(this.registry.voting.call)()
-       resolve(result)
     })
   }
-   async commitVote ({domain, votes, voteOption, salt}) {
-    const challengeId = await this.getChallengeId(domain)
-    const prevPollId = 0
-    const hash = saltHashVote(voteOption, salt)
-     return plcr.commit({pollId: challengeId, hash, tokens: votes, prevPollId})
-  }
-}	}
 
+  // TODO
+  async forceMine (block) {
+    if (this.miningStarted) {
+      await wait(3000)
+      return Promise.resolve()
+    }
+
+    return new Promise(async (resolve, reject) => {
+      // const blockNumber = await this.getCurrentBlockNumber()
+      /*
+      const result = await pify(window.web3.currentProvider.sendAsync)({
+          jsonrpc: '2.0',
+          method: 'evm_mine'
+        })
+      */
+      /*
+      const result = pify(window.web3.currentProvider.sendAsync)({
+          jsonrpc: '2.0',
+          method: 'miner_start',
+          params: 0
+        })
+      */
+
+      this.miningStarted = true
+
+      // resolve(result)
+    })
+  }
+}
 
 export default new RegistryService()
